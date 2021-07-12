0224a8fecc828fa4216e37012e1d630e76ae63f0
Vitali Hauzinski <hauzinski@gmail.com>
2021-07-12 09:55:43 +0300

    fix: fix algorithm code

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-    return matrix.map((value, index) => (index % 2 ? value.sort((a, b) => b - a) : value.sort((a, b) => a - b))).flat(1);
+    return (!matrix) ? [] : matrix.map((value, index) => (index % 2 ? value.sort((a, b) => b - a) : value.sort((a, b) => a - b))).flat(1);
 }
