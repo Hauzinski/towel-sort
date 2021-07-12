ae94037baa911e11992cb6bfa2a8871627c9a1b2
Vitali Hauzinski <hauzinski@gmail.com>
2021-07-12 09:48:46 +0300

    init: start towel-sort task
    fead: add code

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    return matrix.map((value, index) => (index % 2 ? value.sort((a, b) => b - a) : value.sort((a, b) => a - b))).flat(1);
 }
